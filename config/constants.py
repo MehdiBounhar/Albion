b00b9164913b8044eba1fd0f88ee8b63e42bfc6b
MadJack01 <giozdaou@gmail.com>
2025-04-18 16:30:57 +0200

    Ajout des matériaux pour l'artifact foundry

--- config/constants.py
+++ config/constants.py
@@ -21,3 +21,37 @@ CITIES = [
     "Brecilien",
 ]
 BASE_URL = "https://www.albion-online-data.com/api/v2/stats/prices/"
+
+# Rune Item IDs
+RUNE_ITEMS = [
+    "T4_RUNE",
+    "T5_RUNE",
+    "T6_RUNE",
+    "T7_RUNE",
+    "T8_RUNE",
+]
+
+# Artifact Item IDs
+SOUL_ITEMS = [
+    "T4_SOUL",
+    "T5_SOUL",
+    "T6_SOUL",
+    "T7_SOUL",
+    "T8_SOUL",
+]
+
+RELIC_ITEMS = [
+    "T4_RELIC",
+    "T5_RELIC",
+    "T6_RELIC",
+    "T7_RELIC",
+    "T8_RELIC",
+]
+
+AVALONIAN_ITEMS = [
+    "T4_SHARD_AVALONIAN",
+    "T5_SHARD_AVALONIAN",
+    "T6_SHARD_AVALONIAN",
+    "T7_SHARD_AVALONIAN",
+    "T8_SHARD_AVALONIAN",
+]
